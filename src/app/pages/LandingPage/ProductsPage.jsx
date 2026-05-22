04792f4363dccbb6c359faf4913dcd3bebaf2c63
Santhosh1023 <santhoshs0204@gmail.com>
2026-05-22 13:32:12 +0530

    product page image updated

--- src/app/pages/LandingPage/ProductsPage.jsx
+++ src/app/pages/LandingPage/ProductsPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import imgProduct1 from "../../../assets/iris.png";
+import imgProduct1 from "../../../assets/IOT Gateway.png";
 import { MonitorCog, ShieldCheck, Wifi, Zap } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 
